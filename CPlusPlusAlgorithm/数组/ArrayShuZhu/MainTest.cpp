4b653d2244335eeab48f42e9270ba093205f9ba6
“zzborzgit config --global user.email “1450599874@qq.com <1450599874@qq.com>
2024-10-24 11:21:47 +0800

    算法部分

---
+++ CPlusPlusAlgorithm/数组/ArrayShuZhu/MainTest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class IDGenerator
+{
+private:
+    static inline int s_nextID{ 1 };
+
+public:
+    static int getNextID(); // Here's the declaration for a static function
+};
+
+// Here's the definition of the static function outside of the class.  Note we don't use the static keyword here.
+int IDGenerator::getNextID() { return s_nextID++; }
+
+int main()
+{
+    for (int count{ 0 }; count < 5; ++count)
+        std::cout << "The next ID is: " << IDGenerator::getNextID() << '\n';
+
+    return 0;
+}
